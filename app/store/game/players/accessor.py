7259d9f275e16651d5843d0cb4c90e909c3ad17b
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-17 16:01:41 +0300

    Add players accessor

---
+++ app/store/game/players/accessor.py
@@ -0,0 +1,50 @@
+import typing
+from datetime import datetime, date
+from typing import Optional
+from uuid import uuid4
+
+from pymongo.errors import DuplicateKeyError
+
+from app.base.base_accessor import BaseAccessor
+from app.game.models import PlayerModel
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
+    from motor.motor_asyncio import AsyncIOMotorCollection
+
+
+class PlayersAccessor(BaseAccessor):
+    def __init__(self, app: "Application") -> None:
+        super().__init__(app)
+
+    @property
+    def collect(self) -> "AsyncIOMotorCollection":
+        return self.app.mongo.collects.players
+
+    async def connect(self, app: "Application") -> None:
+        pass
+
+    async def disconnect(self, app: "Application") -> None:
+        pass
+
+    async def get_by_vk_id(self, vk_id: int) -> Optional[PlayerModel]:
+        raw_player = await self.collect.find_one({'vk_id': vk_id})
+        if raw_player is not None:
+            return PlayerModel.from_dict(raw_player)
+        return None
+
+    async def add(self, vk_id: int, first_name: str, last_name: str, birthday: Optional[date], city: Optional[str]):
+        model = PlayerModel(
+            _id=uuid4(),
+            vk_id=vk_id,
+            first_name=first_name,
+            last_name=last_name,
+            birthday=birthday,
+            registered_at=datetime.now(),
+            city=city
+        )
+
+        try:
+            await self.collect.insert_one(model.to_dict())
+        except DuplicateKeyError:
+            print('duplicate')
