585a74e22e946b02071d5e62db426d06cdf065ca
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-17 16:04:12 +0300

    Add vkapi-request to get a user-info

--- app/store/vk_api/accessor.py
+++ app/store/vk_api/accessor.py
@@ -7,11 +7,12 @@ import aiohttp
 from aiohttp.client import ClientSession
 
 from app.base.base_accessor import BaseAccessor
-from app.store.vk_api.dataclasses import Message, Update, UpdateObject, UpdateMessage
+from app.store.vk_api.dataclasses import Message, Update, UpdateObject, UpdateMessage, User
 from app.store.vk_api.poller import Poller
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
+    from app.web.config import BotConfig
 
 
 class VkApiAccessor(BaseAccessor):
@@ -22,6 +23,10 @@ class VkApiAccessor(BaseAccessor):
         self.server: Optional[str] = None
         self.poller: Optional[Poller] = None
         self.ts: Optional[int] = None
+
+    @property
+    def cfg(self) -> "BotConfig":
+        return self.app.config.bot
 
     async def connect(self, app: "Application"):
         self.session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
@@ -43,7 +48,10 @@ class VkApiAccessor(BaseAccessor):
         url = host + method + "?"
         if "v" not in params:
             params["v"] = "5.131"
-        url += "&".join([f"{k}={v}" for k, v in params.items()])
+
+        url += '&'.join(
+            f'{k}={v if not isinstance(v, list) else ",".join(tuple(map(str, v)))}' for k, v in params.items())
+
         return url
 
     async def _get_long_poll_service(self):
@@ -97,24 +105,43 @@ class VkApiAccessor(BaseAccessor):
             params=query_params
         )
 
-        pprint(f'{query=}')
+        # pprint(f'{query=}')
 
         async with self.session.get(query) as resp:
             resp = await resp.json()
-            pprint(f'{resp=}')
+            # pprint(f'{resp=}')
 
     async def get_chat(self, peer_id: int) -> dict:
         query = self._build_query(
             host='https://api.vk.com/',
             method='method/messages.getConversationMembers',
             params={
-                'access_token': self.app.config.bot.token,
+                'access_token': self.cfg.token,
                 'peer_id': peer_id,
             }
         )
 
         async with self.session.get(query) as resp:
             return await resp.json()
+
+    async def get_users(self, vk_ids: list[int]) -> list[User]:
+        query = self._build_query(
+            host='https://api.vk.com/',
+            method='method/users.get',
+            params={
+                'access_token': self.cfg.token,
+                'user_ids': vk_ids,
+                'fields': [
+                    'bdate',
+                    'city',
+                ]
+            }
+        )
+
+        async with self.session.get(query) as resp:
+            data = await resp.json()
+
+        return [User.from_dict(u) for u in data['response']]
 
     @staticmethod
     def _pack_updates(raw_updates: dict) -> list[Update]:
--- app/store/vk_api/dataclasses.py
+++ app/store/vk_api/dataclasses.py
@@ -1,6 +1,38 @@
 from dataclasses import dataclass
 from typing import Optional
 from app.game.keyboards import Keyboard
+from datetime import date
+
+
+@dataclass
+class User:
+    vk_id: int
+    first_name: str
+    last_name: str
+    city: Optional[str]
+    birthday: Optional[date] = None
+
+    @staticmethod
+    def from_dict(raw: dict) -> 'User':
+        return User(
+            first_name=raw['first_name'],
+            last_name=raw['last_name'],
+            city=raw['city']['title'] if raw.get('city') else None,
+            birthday=User.parse_vk_date(raw.get('bdate')),
+            vk_id=raw['id']
+        )
+
+    @staticmethod
+    def parse_vk_date(raw: Optional[str]) -> date:
+        if raw is None:
+            return None
+
+        split = raw.split('.')
+
+        if len(split) == 2:
+            return date.fromisoformat(f'--{split[1]}-{split[0]}')
+
+        return date.fromisoformat(f'{split[2]}-{split[1]}-{split[0]}')
 
 
 @dataclass
