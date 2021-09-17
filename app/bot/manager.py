dc6bcc57c06d3ef8473bd4adadc4f42caa0a8563
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-17 16:06:25 +0300

    Rewrite handlers with new dataclass for working with accessors

--- app/bot/manager.py
+++ app/bot/manager.py
@@ -1,11 +1,12 @@
-import typing
-
 from app.base.base_game_accessor import BaseGameAccessor
 from app.game.game import GameCtx
 from app.game.states import BJStates
+from app.store import PlayersAccessor
 from app.store.vk_api.dataclasses import Update
+from app.game.dataclasses import GAccessors
+from typing import TYPE_CHECKING
 
-if typing.TYPE_CHECKING:
+if TYPE_CHECKING:
     from app.web.app import Application
     from app.store.vk_api.accessor import VkApiAccessor
 
@@ -22,18 +23,23 @@ class BotManager:
     def game_accessor(self) -> BaseGameAccessor:
         return self.app.store.game
 
+    @property
+    def player_accessor(self) -> PlayersAccessor:
+        return self.app.store.players
+
     async def handle_updates(self, updates: list[Update]) -> None:
         import app.game.handlers  # DO NOT DELETE
         for update in updates:
             if update.type == 'message_new':
                 msg = update.object.message
-                game_ctx = GameCtx(self.game_accessor, msg.peer_id)
+                game_ctx = GameCtx(self.game_accessor, msg.peer_id, msg)
+                game_accessors = GAccessors(self.vk_api, self.player_accessor)
 
                 async with game_ctx.proxy() as ctx:
                     if ctx.state is None:
                         ctx.state = BJStates.WAITING_FOR_TRIGGER
 
-                    await ctx.state.handler(self.vk_api, ctx, msg)
+                    await ctx.state.handler(ctx, game_accessors)
 
 
 def setup(app: "Application") -> None:
--- app/game/handlers.py
+++ app/game/handlers.py
@@ -1,43 +1,49 @@
+from typing import TYPE_CHECKING
+
 from app.game.deck import Card
 from app.game.game import GameCtxProxy, BlackJackGame
+from app.game.models import PlayerModel
 from app.game.player import Player
 from app.game.states import BJStates
-from app.game.utils import parse_bet_expr, get_username_by_id, get_payload
-from app.store.vk_api.dataclasses import Message, UpdateMessage
+from app.game.utils import parse_bet_expr, get_payload
+from app.store.vk_api.dataclasses import Message
+
+if TYPE_CHECKING:
+    from app.game.dataclasses import GAccessors
 
 START_MSG = '/go'
 
 
 @BJStates.WAITING_FOR_TRIGGER.register
-async def trigger_received(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    if START_MSG in msg.text:
+async def trigger_received(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    if START_MSG in ctx.msg.text:
         answer = 'Сыграем?'
-        await vk_api.send_message(
-            Message(peer_id=msg.peer_id, text=answer, kbd=ctx.state.keyboard, photos=Card.joker_photo))
+        await access.vk.send_message(
+            Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard, photos=Card.joker_photo))
         ctx.state = BJStates.WAITING_FOR_NEW_GAME
 
 
 @BJStates.WAITING_FOR_NEW_GAME.register
-async def new_game_clicked(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    if (payload := get_payload(msg)) is None:
+async def new_game_clicked(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    if (payload := get_payload(ctx.msg)) is None:
         return
 
     if check_cancel(payload):
-        await do_cancel(vk_api, ctx, msg)
+        await do_cancel(ctx, access)
         return
 
     if payload == 'new_game':
         answer = 'Выберите количество игроков'
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, kbd=ctx.state.keyboard))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard))
         ctx.state = BJStates.WAITING_FOR_PLAYERS_COUNT
 
 
-async def do_cancel(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    await hide_keyboard(vk_api, ctx, msg, 'Отмена игры')
-    await do_end(vk_api, ctx, msg)
+async def do_cancel(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    await hide_keyboard(ctx, access, 'Отмена игры')
+    await do_end(ctx, access)
 
 
-async def do_end(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
+async def do_end(ctx: GameCtxProxy, access: 'GAccessors') -> None:
     del ctx.state
     del ctx.game
 
@@ -51,143 +57,176 @@ def check_cancel(payload: str) -> bool:
 
 
 @BJStates.WAITING_FOR_PLAYERS_COUNT.register
-async def player_numbers_clicked(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    if (payload := get_payload(msg)) is None:
+async def player_numbers_clicked(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    if (payload := get_payload(ctx.msg)) is None:
         return
 
     if check_cancel(payload):
-        await do_cancel(vk_api, ctx, msg)
+        await do_cancel(ctx, access)
         return
 
     choice = int(payload)
     if choice in (1, 2, 3):
-        await hide_keyboard(vk_api, ctx, msg, f'Количество игроков: {choice}')
+        await hide_keyboard(ctx, access, f'Количество игроков: {choice}')
 
         answer = 'Отлично! Чтобы зарегистрироваться на игру, желающие должны нажать кнопку:'
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, kbd=ctx.state.keyboard))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard))
 
-        ctx.game = BlackJackGame(chat_id=msg.peer_id, num_of_players=choice)
+        ctx.game = BlackJackGame(chat_id=ctx.msg.peer_id, num_of_players=choice)
         ctx.state = BJStates.WAITING_FOR_REGISTER
 
 
-# And hide keyboard
-async def hide_keyboard(vk_api, _: GameCtxProxy, msg: UpdateMessage, text: str) -> None:
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=text))
+async def hide_keyboard(ctx: GameCtxProxy, access: 'GAccessors', text: str) -> None:
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=text))
+
+
+async def handle_userinfo(access: 'GAccessors', uid: int) -> PlayerModel:
+    db_user_data = await access.players.get_by_vk_id(uid)
+    if db_user_data is None:
+        vk_user_data = (await access.vk.get_users([uid]))[0]
+        await access.players.add(
+            vk_id=vk_user_data.vk_id,
+            first_name=vk_user_data.first_name,
+            last_name=vk_user_data.last_name,
+            birthday=vk_user_data.birthday,
+            city=vk_user_data.city,
+        )
+    return await access.players.get_by_vk_id(uid)
 
 
 @BJStates.WAITING_FOR_REGISTER.register
-async def player_registered(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    if (payload := get_payload(msg)) is None:
+async def player_registered(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    if (payload := get_payload(ctx.msg)) is None:
         return
 
     if check_cancel(payload):
-        await do_cancel(vk_api, ctx, msg)
+        await do_cancel(ctx, access)
         return
 
     if payload == 'register':
-        chat = await vk_api.get_chat(msg.peer_id)  # TODO chat to dataclass
-        sender_name = get_username_by_id(msg.from_id, chat)
+        db_player_data = await handle_userinfo(access, ctx.msg.from_id)
+        new_player = Player(name=db_player_data.first_name, vk_id=db_player_data.vk_id)
 
-        new_player = Player(sender_name, msg.from_id)
         if ctx.game.add_player(new_player):
             answer = f'{new_player}, ты в игре! (Зарегистрировано: {ctx.game.ratio_of_registered})'
         else:
             answer = f'{new_player}, ты уже в игре. Дождись остальных игроков!'
 
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, kbd=ctx.state.keyboard))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard))
 
         if ctx.game.all_players_registered:
-            await complete_registration(vk_api, ctx, msg)
+            await complete_registration(ctx, access)
 
 
-async def complete_registration(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def complete_registration(ctx: GameCtxProxy, access: 'GAccessors') -> None:
     answer = 'Все игроки зарегистрированы. Игра начинается! %0A Укажите сумму ставки без пробелов.'
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer))
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
     ctx.state = BJStates.WAITING_FOR_BETS
 
 
 @BJStates.WAITING_FOR_BETS.register
-async def bet_placed(vk_api, ctx: GameCtxProxy, msg: UpdateMessage) -> None:
-    if (player := ctx.game.get_player_by_id(msg.from_id)) is not None:
-        if (bet := parse_bet_expr(msg.text)) is None:
+async def bet_placed(ctx: GameCtxProxy, access: 'GAccessors') -> None:
+    if (player := ctx.game.get_player_by_id(ctx.msg.from_id)) is not None:
+        if (bet := parse_bet_expr(ctx.msg.text)) is None:
             answer = f'{player}, вы указали некорректную сумму ставки'
         else:
             answer = f'{player}, ваша ставка принята! Сумма ставки: {bet}'
             player.place_bet(bet)
 
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
 
         if ctx.game.all_players_bet:
-            await complete_betting(vk_api, ctx, msg)
+            await complete_betting(ctx, access)
 
 
-async def complete_betting(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def complete_betting(ctx: GameCtxProxy, access: 'GAccessors'):
     answer = 'Все игроки поставили ставки! Начинаю раздачу карт...'
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer))
-    await hand_out_cards(vk_api, ctx, msg)
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
+    await hand_out_cards(ctx, access)
 
 
-async def hand_out_cards(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def hand_out_cards(ctx: GameCtxProxy, access: 'GAccessors'):
     ctx.game.deal_cards()
 
     for player in ctx.game.players_and_dealer:
         answer = f'{player}, Сумма очков: {player.score}'
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, photos=player.cards_photos))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, photos=player.cards_photos))
 
-    await ask_player(vk_api, ctx, msg)
+    await ask_player(ctx, access)
 
 
-async def ask_player(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def ask_player(ctx: GameCtxProxy, access: 'GAccessors'):
     player = ctx.game.current_player
     answer = f'{player}, Сумма очков: {player.score} Ваш выбор?'
-    ctx.state = BJStates.WAITING_FOR_ACTION  # Here, 'cause need to use state.keyboard
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, kbd=ctx.state.keyboard))
+    ctx.state = BJStates.WAITING_FOR_ACTION  # because need to use state.keyboard
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard))
 
 
 @BJStates.WAITING_FOR_ACTION.register
-async def handling_action(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def handling_action(ctx: GameCtxProxy, access: 'GAccessors'):
     player = ctx.game.current_player
-    # TODO Is it work normal?
     choose = {
-        'hit': lambda: handle_hit_action(vk_api, ctx, msg, player),
-        'stand': lambda: handle_next_player(vk_api, ctx, msg),
+        'hit': lambda: handle_hit_action(ctx, access, player),
+        'stand': lambda: handle_next_player(ctx, access),
     }
 
     # TODO add choose-object for player
-    if (choice := get_payload(msg)) in ('hit', 'stand') and msg.from_id == player.vk_id:
+    if (choice := get_payload(ctx.msg)) in ('hit', 'stand') and ctx.msg.from_id == player.vk_id:
         await choose[choice]()
 
 
-async def handle_hit_action(vk_api, ctx: GameCtxProxy, msg: UpdateMessage, player: Player) -> None:
+async def handle_hit_action(ctx: GameCtxProxy, access: 'GAccessors', player: Player) -> None:
     card = ctx.game.deck.get_card()
     player.add_card(card)
 
     answer = f'{player}, твои карты:'
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, photos=player.cards_photos))
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, photos=player.cards_photos))
 
     if player.in_game:
-        await ask_player(vk_api, ctx, msg)
+        await ask_player(ctx, access)
     else:
         answer = f'{player}, перебор! (Сумма: {player.score})'
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer))
-        await handle_next_player(vk_api, ctx, msg)
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
+        await handle_next_player(ctx, access)
 
 
-async def handle_next_player(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def handle_next_player(ctx: GameCtxProxy, access: 'GAccessors'):
     if ctx.game.next_player():
-        await ask_player(vk_api, ctx, msg)
+        await ask_player(ctx, access)
     else:
         ctx.game.handle_dealer()
-        await show_results(vk_api, ctx, msg)
+        await show_results(ctx, access)
 
 
-async def show_results(vk_api, ctx: GameCtxProxy, msg: UpdateMessage):
+async def show_results(ctx: GameCtxProxy, access: 'GAccessors'):
     dealer = ctx.game.dealer
     answer = f'Дилер, Сумма очков: {dealer.score}'
-    await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer, photos=dealer.cards_photos))
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, photos=dealer.cards_photos))
 
     for player in ctx.game.players:
         answer = f'{player}, {ctx.game.define_result(player).value}'
-        await vk_api.send_message(Message(peer_id=msg.peer_id, text=answer))
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
 
-    await do_end(vk_api, ctx, msg)
+    await play_again_request(ctx, access)
+
+
+async def play_again_request(ctx: GameCtxProxy, access: 'GAccessors'):
+    answer = f'Выбирай'
+    ctx.state = BJStates.WAITING_FOR_ANSWER_TO_REPEAT_QUESTION
+    await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer, kbd=ctx.state.keyboard))
+
+
+@BJStates.WAITING_FOR_ANSWER_TO_REPEAT_QUESTION.register
+async def handle_play_again_response(ctx: GameCtxProxy, access: 'GAccessors'):
+    if (payload := get_payload(ctx.msg)) is None:
+        return
+
+    if payload == 'stop':
+        answer = f'Окей, больше не играем!'
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
+        await do_end(ctx, access)
+    else:
+        answer = f'Круто, играем снова! Укажите сумму ставки без пробелов.'
+        await access.vk.send_message(Message(peer_id=ctx.msg.peer_id, text=answer))
+        ctx.game.reset()
+        ctx.state = BJStates.WAITING_FOR_BETS
