84c4a4f7ed55a3f90e70ed82cd810104dc1c0716
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-18 03:17:16 +0300

    add init player and game-settings accessors

--- app/store/__init__.py
+++ app/store/__init__.py
@@ -1,21 +1,22 @@
 from typing import TYPE_CHECKING
 
-from app.store.game.players.accessor import PlayersAccessor
-
 if TYPE_CHECKING:
-    from app.web.app import Application
+    from app.app import Application
 
 
 class Store:
-    def __init__(self, app: "Application"):
+    def __init__(self, app: 'Application'):
+        from app.store.players.accessor import PlayersAccessor
         from app.store.vk_api.accessor import VkApiAccessor
         from app.store.admin.accessor import AdminAccessor
         from app.store.game.redis_accessor import RedisGameAccessor
+        from app.store.game_settings.accessor import GameSettingsAccessor
 
+        self.game_settings = GameSettingsAccessor(app)
         self.admins = AdminAccessor(app)
         self.vk_api = VkApiAccessor(app)
-        self.game = RedisGameAccessor(app)
         self.players = PlayersAccessor(app)
+        self.game = RedisGameAccessor(app)
 
 
 def setup_store(app: "Application"):
