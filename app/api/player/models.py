fd2c8fe1c17b81ed8d3502fd27896d3c11c927fa
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-18 03:18:26 +0300

    Add some attributes to player-model

--- app/api/player/models.py
+++ app/api/player/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, date, timedelta
 from dataclasses import dataclass
 from typing import Optional
 from uuid import UUID
@@ -11,8 +11,10 @@ class PlayerModel:
     first_name: str
     last_name: str
     city: str
-    registered_at: datetime.datetime
-    birthday: Optional[datetime.date] = None
+    cash: float
+    last_bonus_date: datetime
+    registered_at: datetime
+    birthday: Optional[date] = None
 
     def to_dict(self) -> dict:
         return {
@@ -21,8 +23,10 @@ class PlayerModel:
             'first_name': self.first_name,
             'last_name': self.last_name,
             'registered_at': str(self.registered_at),
+            'last_bonus_date': str(self.last_bonus_date),
             'birthday': str(self.birthday),
             'city': self.city,
+            'cash': self.cash,
         }
 
     @staticmethod
@@ -32,8 +36,14 @@ class PlayerModel:
             vk_id=raw['vk_id'],
             first_name=raw['first_name'],
             last_name=raw['last_name'],
-            registered_at=raw['registered_at'],
-            birthday=raw['birthday'],
-            city=raw['city']
+            registered_at=datetime.fromisoformat(raw['registered_at']),
+            last_bonus_date=datetime.fromisoformat(raw['last_bonus_date']),
+            birthday=date.fromisoformat(raw['birthday']),
+            city=raw['city'],
+            cash=raw['cash'],
         )
+
+    def check_bonus(self, minutes: int) -> bool:
+        return datetime.now() - self.last_bonus_date > timedelta(minutes=minutes)
+
 
