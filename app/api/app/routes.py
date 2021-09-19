66caa091c064b74a592a169707faae14b1994485
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-20 00:49:35 +0300

    add game_settings route in routes

--- app/api/app/routes.py
+++ app/api/app/routes.py
@@ -5,7 +5,9 @@ def setup_routes(app: Application):
     from app.api.admin.routes import setup_routes as admin_setup_routes
     from app.api.players.routes import setup_routes as players_setup_routes
     from app.api.chats.routes import setup_routes as chats_setup_routes
+    from app.api.game_settings.routes import setup_routes as game_settings_setup_routes
 
     admin_setup_routes(app)
     players_setup_routes(app)
     chats_setup_routes(app)
+    game_settings_setup_routes(app)
