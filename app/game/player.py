50bfb3dbc87c2fbb82c63307f6c1b7543a65ec6d
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-19 23:54:58 +0300

    Add PlayerStatus Enum & return card-captions instead of pictures

--- app/game/player.py
+++ app/game/player.py
@@ -1,6 +1,16 @@
+from enum import Enum
 from typing import Optional
 
 from app.game.deck import Card
+
+
+class PlayerStatus(str, Enum):
+    DEFEAT = 'Проигрыш'
+    DRAW = 'Ничья'
+    WIN = 'Победа'
+    IN_GAME = 'В игре'
+    BUST = 'Перебор'
+    BJ = 'Блэкджек'
 
 
 class Player:
@@ -19,6 +29,7 @@ class Player:
             self._bet: Optional[float] = None
             self._cards: list[Card] = []
             self._score: int = 0
+            self._status = PlayerStatus.IN_GAME
 
     def from_dict(self, raw: dict) -> None:
         self._name = raw['name']
@@ -27,6 +38,7 @@ class Player:
         self._bet = raw['bet']
         self._cards = [Card(d=card_info) for card_info in raw['cards']]
         self._score = raw['score']
+        self._status = PlayerStatus(raw['status'])
 
     def to_dict(self) -> dict:
         return {
@@ -36,7 +48,26 @@ class Player:
             'cash': self.cash,
             'cards': [c.to_dict() for c in self._cards],
             'score': self.score,
+            'status': self._status
         }
+
+    def update_cash(self):
+        if self.status is PlayerStatus.WIN:
+            self.cash += self.bet
+        elif self.status in (PlayerStatus.DEFEAT, PlayerStatus.BUST):
+            self.cash -= self.bet
+
+    @property
+    def status(self) -> PlayerStatus:
+        return self._status
+
+    @status.setter
+    def status(self, value: PlayerStatus) -> None:
+        self._status = value
+
+    @property
+    def is_dealer(self) -> bool:
+        return self._vk_id is None
 
     @property
     def cash(self) -> float:
@@ -51,30 +82,33 @@ class Player:
         self._score = 0
         self._cards.clear()
 
+    def set_bust(self):
+        self.status = PlayerStatus.BUST
+        self.update_cash()
+
     @property
-    def in_game(self) -> bool:
+    def not_bust(self) -> bool:
         return self.score <= 21
 
     def add_card(self, card: Card) -> None:
         self._cards.append(card)
         self._score += card.bj_value(self._score)
 
-    @property
-    def cards_photos(self) -> str:
-        res = ','.join([card.photo for card in self._cards])
-
-        if self._vk_id == -1 and len(self._cards) == 1:  # Dealer
-            res += f',{Card.sharp_photo}'
-
-        return res
+    # @property
+    # def cards_photos(self) -> str:
+    #     res = ','.join([card.photo for card in self._cards])
+    #
+    #     if self.is_dealer and len(self._cards) == 1:
+    #         res += f',{Card.sharp_photo}'
+    #
+    #     return res
 
     # deprecated
     @property
-    def cards(self) -> str:
-        ans = ''
-        for card in self._cards:
-            ans += str(card)
-
+    def cards_info(self) -> str:
+        ans = '%0A'.join([c.card_txt for c in self._cards])
+        if self.is_dealer and len(self._cards) == 1:
+            ans += f'%0A{Card.fake_card()}'
         return ans
 
     def place_bet(self, value: int) -> None:
@@ -100,7 +134,7 @@ class Player:
         return self.vk_id == other.vk_id
 
     def __str__(self) -> str:
-        if self._vk_id == -1:
+        if self.is_dealer:
             return 'Дилер'
 
         return f'[id{self.vk_id}|{self.name}]'
