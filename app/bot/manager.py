b58703b41f1cd356f17577023a0284ab5757f8eb
d_ivahnencko <ivahnencko01@gmail.com>
2021-09-24 14:11:46 +0300

    add prints for exceptions

--- app/bot/manager.py
+++ app/bot/manager.py
@@ -46,6 +46,8 @@ class BotManager:
                         await ctx.state.handler(ctx, game_accessors)
                     except Exception as e:
                         print(e)
+                        print(e.args)
+                        print(e.__traceback__)
 
 def setup(app: "Application") -> None:
     app.bot_manager = BotManager(app)
